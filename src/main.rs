13dde4474f3d9bad730a4b37c860d1ff55ca6fce
Daniel Pavela <daniel.pavela@gmail.com>
2020-08-01 14:50:10 +1000

    Make progress bar functional

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate gio;
 
 use gtk::prelude::*;
 use gio::prelude::*;
-
+use glib::clone;
 use gtk::{Application, ApplicationWindow, Button};
+use rand::Rng;
 
 fn main() {
     let application = Application::new(
@@ -19,25 +20,29 @@ fn main() {
 		 window.set_border_width(20);
 
 		let grid = gtk::Grid::new();
+		grid.set_row_spacing(10);
 
 		let title = gtk::Label::new(Some(""));
 		title.set_markup("<span font_desc=\"20.0\">Ram Downloader</span>");
 
 		let subtitle = gtk::Label::new(Some(""));
-		subtitle.set_markup("<span font_desc=\"8.0\">By Daniel Pavela</span>");
+		subtitle.set_markup("<span font_desc=\"8.0\">By Daniel</span>");
 
 		let ramProtocolSelectorOne = gtk::RadioButton::with_label("CloudRAM v1.1");
 		let ramProtocolSelectorTwo = gtk::RadioButton::with_label_from_widget(&ramProtocolSelectorOne, "CloudRAM v2.0");
-
-        let button = Button::with_label("Download RAM");
-        button.connect_clicked(|_| {
-            println!("Clicked!");
-        });
 
 		let progress = gtk::ProgressBar::new();
 		progress.set_text(Some("Progress"));
 		progress.set_show_text(true);
 		progress.set_hexpand(true);
+		//progress.set_fraction(0.3);
+
+		let button = Button::with_label("Download RAM");
+		button.connect_clicked(clone!(@weak progress => move |_| {
+			let mut rng = rand::thread_rng();
+			let currentProgress = progress.get_fraction();
+			progress.set_fraction(currentProgress + rng.gen_range(0.1, 0.3));
+		}));
 
 		grid.attach(&title, 0, 0, 2, 1);
 		grid.attach(&subtitle, 0, 1, 2, 1);
